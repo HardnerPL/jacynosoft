16adf22f4a4cd344ce947c30d70cd7fe9b4e9259
Adam Jacyno <jacynoadam@gmail.com>
2020-01-02 20:56:21 +0100

    refactor: remove obsolete templates and classes, replace them with new designed ones

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,31 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hardner07@gmail.com
- * Date: 5/13/2019
- * Time: 4:25 PM
- */
-
-?>
-<div class="container">
-    <div id="nav" class="text-center">
-        <a href="#" class="link">O mnie</a>
-        <a href="#" class="link">Portfolio</a>
-        <a href="#" class="link">Kontakt</a>
-    </div>
-    <hr>
-    <div class="text-center">
-        <img id="profile-img" src="assets/img/profile.jpg" alt="Profile image">
-    </div>
-    <div id="about-header" class="text-center">
-        <span>Jacyno</span><span class="text-blue medium">SOFT</span>
-    </div>
-        <div id="about-content" class="mx-auto text-center">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus diam risus, ultricies sit amet tincidunt
-            nec, vehicula in velit. Mauris feugiat lectus quis elit egestas rhoncus. Aliquam erat volutpat. Nulla ut
-            magna nibh.</p>
-        <p>Nulla dapibus dictum sem et eleifend. Cras non tortor et diam viverra egestas. Donec eu dui ante.
-            Nunc a cursus turpis, fermentum faucibus nibh. Curabitur feugiat urna orci, id mollis mi porta ut. In
-            ullamcorper pharetra velit accumsan dapibus. Nam elementum sem id mi luctus euismod. Donec in tempus
-            tortor, vitae aliquam odio. </p>
-        </div>
+<div class="navbar navbar-dark navbar-expand-sm fixed-top">
+	<div class="container p-3">
+		<a class="navbar-brand" href="">
+			JacynoSOFT
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-end" id="navbar">
+			<ul class="navbar-nav text-right">
+				<li class="nav-item active">
+					<a class="mx-2 nav-link" href="">Start</a>
+				</li>
+				<li class="nav-item active">
+					<a class="mx-2 nav-link" href="">O mnie</a>
+				</li>
+				<li class="nav-item active">
+					<a class="mx-2 nav-link" href="">Projekty</a>
+				</li>
+				<li class="nav-item active">
+					<a class="mx-2 nav-link" href="">Kontakt</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div id="motto-container" class="row">
+	<h1 id="motto-text">
+		Fast. Minimalistic. Stylish.
+	</h1>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,9 +3,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title><?= $title ?></title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
         <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="assets/css/all.css" rel="stylesheet">
         <link href="assets/css/main.css" rel="stylesheet" type="text/css">
     </head>
     <body>
+	<div class="container-fluid">
