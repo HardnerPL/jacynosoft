6edcf04ed11c531d128d6e6443dcfd26616b0ad0
Adam Jacyno <jacynoadam@gmail.com>
2020-01-02 23:35:05 +0100

    feat: add 'technology' section and make it responsive

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -74,7 +74,8 @@
 			euismod, iaculis orci vel, vestibulum metus.
 		</p>
 	</div>
-	<div class="col-10 offset-1 col-sm-8 offset-sm-2 col-md-4 offset-md-2 col-lg-3 offset-lg-2 mt-5 mt-md-0 text-center">
+	<div
+		class="col-10 offset-1 col-sm-8 offset-sm-2 col-md-4 offset-md-2 col-lg-3 offset-lg-2 mt-5 mt-md-0 text-center">
 		<h1>Front end</h1>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -91,5 +92,60 @@
 			nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
 			euismod, iaculis orci vel, vestibulum metus.
 		</p>
+	</div>
+</div>
+<hr class="w-75">
+<div class="row my-5">
+	<div class="col-12 col-sm-10 col-md-8 col-xl-6 offset-sm-1 offset-md-2 offset-xl-3">
+		<div class="row">
+			<div class="col text-center">
+				<h1>Technologie</h1>
+			</div>
+		</div>
+		<div class="row mt-4">
+			<div class="col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
+				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
+			</div>
+			<div class="col-8 offset-2 col-sm offset-sm-0 p-2">
+				<h3>MVC Frameworks</h3>
+				<p class="my-auto">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
+					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
+					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
+					nunc
+					euismod, iaculis orci vel, vestibulum metus.
+				</p>
+			</div>
+		</div>
+		<div class="row mt-4">
+			<div class="order-1 order-sm-0 col-8 offset-2 col-sm offset-sm-0 p-2">
+				<h3>MVC Frameworks</h3>
+				<p class="my-auto">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
+					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
+					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
+					nunc
+					euismod, iaculis orci vel, vestibulum metus.
+				</p>
+			</div>
+			<div class="order-0 col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center">
+				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
+			</div>
+		</div>
+		<div class="row mt-4">
+			<div class="col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
+				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
+			</div>
+			<div class="col-8 offset-2 col-sm offset-sm-0 p-2">
+				<h3>MVC Frameworks</h3>
+				<p class="my-auto">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
+					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
+					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
+					nunc
+					euismod, iaculis orci vel, vestibulum metus.
+				</p>
+			</div>
+		</div>
 	</div>
 </div>
