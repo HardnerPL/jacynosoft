04bc655b328fccbb9bb38585631f8c5026bffabe
Adam Jacyno <jacynoadam@gmail.com>
2020-01-02 21:46:31 +0100

    feat: add 'about me' row and make it responsive

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,4 +1,4 @@
-<div class="navbar navbar-dark navbar-expand-sm fixed-top">
+<div class="navbar navbar-dark navbar-expand-sm fixed-top text-light">
 	<div class="container p-3">
 		<a class="navbar-brand" href="">
 			JacynoSOFT
@@ -24,8 +24,30 @@
 		</div>
 	</div>
 </div>
-<div id="motto-container" class="row">
+<div id="motto-container" class="row text-light">
 	<h1 id="motto-text">
 		Fast. Minimalistic. Stylish.
 	</h1>
+</div>
+<div class="row">
+	<div class="col-12 col-sm-10 col-md-8 col-xl-6 offset-sm-1 offset-md-2 offset-xl-3 my-3">
+		<div class="row">
+			<div class="col text-center">
+				<h1>O mnie</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
+				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
+			</div>
+			<div class="col-8 offset-2 col-sm offset-sm-0 p-2 content-center">
+				<p class="my-auto">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
+					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
+					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
+					euismod, iaculis orci vel, vestibulum metus.
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
-</div>
 <div id="footer" class="bg-gray text-center py-3">
     © Jacynosoft 2019
+</div>
 </div>
 <script src="assets/js/jquery.js"></script>
 <script src="assets/js/bootstrap.js"></script>
