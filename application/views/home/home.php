d0179d061ad7118c60b1ef8a9f04f46eeb8c4d2f
Adam Jacyno <jacynoadam@gmail.com>
2020-01-03 14:53:27 +0100

    refactor: minor responsiveness improvements

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -25,7 +25,7 @@
 	</div>
 </div>
 <div id="motto-container" class="row text-light">
-	<h1 id="motto-text">
+	<h1 id="motto-text" class="text-center">
 		Fast. Minimalistic. Stylish.
 	</h1>
 </div>
@@ -40,8 +40,8 @@
 			<div class="col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
 				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
 			</div>
-			<div class="col-8 offset-2 col-sm offset-sm-0 p-2 content-center">
-				<p class="my-auto">
+			<div class="col-12 px-4 px-sm-0 col-sm offset-sm-0 p-2 content-center">
+				<p class="my-auto text-center text-sm-left">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
 					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
 					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
@@ -53,7 +53,7 @@
 </div>
 <hr class="w-75">
 <div class="row my-5">
-	<div class="col-10 offset-1 col-sm-8 offset-sm-2 col-lg-6 offset-lg-3 text-center">
+	<div class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-lg-6 offset-lg-3 text-center">
 		<h1>Doświadczenie</h1>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -65,7 +65,7 @@
 </div>
 <hr class="w-25">
 <div class="row my-5">
-	<div class="col-10 offset-1 col-sm-8 offset-sm-2 col-md-4 offset-md-1 col-lg-3 offset-lg-2 text-center">
+	<div class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-md-4 offset-md-1 col-lg-3 offset-lg-2 text-center">
 		<h1>Back end</h1>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -75,7 +75,7 @@
 		</p>
 	</div>
 	<div
-		class="col-10 offset-1 col-sm-8 offset-sm-2 col-md-4 offset-md-2 col-lg-3 offset-lg-2 mt-5 mt-md-0 text-center">
+		class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-md-4 offset-md-2 col-lg-3 offset-lg-2 mt-5 mt-md-0 text-center">
 		<h1>Front end</h1>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -84,7 +84,7 @@
 			euismod, iaculis orci vel, vestibulum metus.
 		</p>
 	</div>
-	<div class="col-10 offset-1 col-sm-8 offset-sm-2 col-md-4 offset-md-4 mt-5 text-center">
+	<div class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-md-4 offset-md-4 mt-5 text-center">
 		<h1>UI/UX</h1>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -106,7 +106,7 @@
 			<div class="col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
 				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
 			</div>
-			<div class="col-8 offset-2 col-sm offset-sm-0 p-2">
+			<div class="col-8 offset-2 col-sm offset-sm-0 p-2 text-center text-sm-left">
 				<h3>MVC Frameworks</h3>
 				<p class="my-auto">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -118,7 +118,7 @@
 			</div>
 		</div>
 		<div class="row mt-4">
-			<div class="order-1 order-sm-0 col-8 offset-2 col-sm offset-sm-0 p-2">
+			<div class="order-1 order-sm-0 col-8 offset-2 col-sm offset-sm-0 p-2 text-center text-sm-left">
 				<h3>MVC Frameworks</h3>
 				<p class="my-auto">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
@@ -136,7 +136,7 @@
 			<div class="col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
 				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
 			</div>
-			<div class="col-8 offset-2 col-sm offset-sm-0 p-2">
+			<div class="col-8 offset-2 col-sm offset-sm-0 p-2 text-center text-sm-left">
 				<h3>MVC Frameworks</h3>
 				<p class="my-auto">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
