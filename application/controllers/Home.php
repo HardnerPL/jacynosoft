eba8b34a2d082f0cfd46f4da31476ab9800f4d81
Adam Jacyno <jacynoadam@gmail.com>
2020-01-04 20:35:45 +0100

    style: reindented all files

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,17 +3,17 @@
 class Home extends CI_Controller
 {
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    public function index()
-    {
+	public function index()
+	{
 
-        $data['title'] = "Home | JacynoSOFT";
-        $this->load->view('templates/header', $data);
-        $this->load->view('home/home');
-        $this->load->view('templates/footer');
-    }
+		$data['title'] = "Home | JacynoSOFT";
+		$this->load->view('templates/header', $data);
+		$this->load->view('home/home');
+		$this->load->view('templates/footer');
+	}
 }
