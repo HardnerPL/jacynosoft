dfe06980f7dd81b006d533178bf97bb455e602a7
Adam Jacyno <jacynoadam@gmail.com>
2020-01-14 21:03:20 +0100

    Replace all texts with their  references

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -53,41 +53,29 @@
 	<div class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-lg-6 offset-lg-3 text-center">
 		<h1><?= get_property($lang,'experience_title') ?></h1>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-			efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-			nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
-			euismod, iaculis orci vel, vestibulum metus.
+			<?= get_property($lang,'experience_content') ?>
 		</p>
 	</div>
 </div>
 <hr class="w-25">
 <div class="row my-5">
 	<div class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-md-4 offset-md-1 col-lg-3 offset-lg-2 text-center">
-		<span class="experience-header px-2">Back end</span>
+		<span class="experience-header px-2"><?= get_property($lang,'back_end_title') ?></span>
 		<p class="mt-2">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-			efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-			nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
-			euismod, iaculis orci vel, vestibulum metus.
+			<?= get_property($lang,'back_end_content') ?>
 		</p>
 	</div>
 	<div
 		class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-md-4 offset-md-2 col-lg-3 offset-lg-2 mt-5 mt-md-0 text-center">
-		<span class="experience-header px-2">Front end</span>
+		<span class="experience-header px-2"><?= get_property($lang,'front_end_title') ?></span>
 		<p class="mt-2">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-			efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-			nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
-			euismod, iaculis orci vel, vestibulum metus.
+			<?= get_property($lang,'front_end_content') ?>
 		</p>
 	</div>
 	<div class="col-12 px-4 px-sm-0 col-sm-8 offset-sm-2 col-md-4 offset-md-4 mt-5 text-center">
-		<span class="experience-header px-2">UI/UX</span>
+		<span class="experience-header px-2"><?= get_property($lang,'ui_ux_title') ?></span>
 		<p class="mt-2">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-			efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-			nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel nunc
-			euismod, iaculis orci vel, vestibulum metus.
+			<?= get_property($lang,'ui_ux_content') ?>
 		</p>
 	</div>
 </div>
@@ -96,7 +84,7 @@
 	<div class="col-12 col-sm-10 col-md-8 col-xl-6 offset-sm-1 offset-md-2 offset-xl-3">
 		<div class="row">
 			<div class="col text-center">
-				<h1>Technologie</h1>
+				<h1><?= get_property($lang,'technologies_title') ?></h1>
 			</div>
 		</div>
 		<div class="row mt-4">
@@ -104,25 +92,17 @@
 				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
 			</div>
 			<div class="col-8 offset-2 col-sm offset-sm-0 p-2 text-center text-sm-left">
-				<h3>MVC Frameworks</h3>
+				<h3><?= get_property($lang,'technology_one_title') ?></h3>
 				<p class="my-auto">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
-					nunc
-					euismod, iaculis orci vel, vestibulum metus.
+					<?= get_property($lang,'technology_one_content') ?>
 				</p>
 			</div>
 		</div>
 		<div class="row mt-4">
 			<div class="order-1 order-sm-0 col-8 offset-2 col-sm offset-sm-0 p-2 text-center text-sm-left">
-				<h3>MVC Frameworks</h3>
+				<h3><?= get_property($lang,'technology_two_title') ?></h3>
 				<p class="my-auto">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
-					nunc
-					euismod, iaculis orci vel, vestibulum metus.
+					<?= get_property($lang,'technology_two_content') ?>
 				</p>
 			</div>
 			<div class="order-0 col-6 offset-3 offset-sm-0 col-sm-4 col-md-3 p-2 mr-4 content-center my-auto">
@@ -134,13 +114,9 @@
 				<img class="rounded-circle w-100" src="assets/img/profile.jpg">
 			</div>
 			<div class="col-8 offset-2 col-sm offset-sm-0 p-2 text-center text-sm-left">
-				<h3>MVC Frameworks</h3>
+				<h3><?= get_property($lang,'technology_three_title') ?></h3>
 				<p class="my-auto">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
-					nunc
-					euismod, iaculis orci vel, vestibulum metus.
+					<?= get_property($lang,'technology_three_content') ?>
 				</p>
 			</div>
 		</div>
@@ -151,38 +127,26 @@
 	<div class="col-12 px-4 px-sm-0 col-sm-10 offset-sm-1">
 		<div class="row">
 			<div class="col">
-				<h1 class="text-center mb-4">Oferta</h1>
+				<h1 class="text-center mb-4"><?= get_property($lang,'offer_title') ?></h1>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-12 my-2 my-lg-0 col-lg px-3 text-center py-3 offer-container">
-				<span class="offer-header">Strony internetowe</span>
+				<span class="offer-header"><?= get_property($lang,'offer_one_title') ?></span>
 				<p class="my-auto">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
-					nunc
-					euismod, iaculis orci vel, vestibulum metus.
+					<?= get_property($lang,'offer_one_content') ?>
 				</p>
 			</div>
 			<div class="col-12 my-2 my-lg-0 col-lg px-3 text-center py-3 offer-container">
-				<span class="offer-header">Systemy CMS</span>
+				<span class="offer-header"><?= get_property($lang,'offer_two_title') ?></span>
 				<p class="my-auto">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
-					nunc
-					euismod, iaculis orci vel, vestibulum metus.
+					<?= get_property($lang,'offer_two_content') ?>
 				</p>
 			</div>
 			<div class="col-12 my-2 my-lg-0 col-lg px-3 text-center py-3">
-				<span class="offer-header">Hosting & Domeny</span>
+				<span class="offer-header"><?= get_property($lang,'offer_three_title') ?></span>
 				<p class="my-auto">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, nisi sit amet iaculis
-					efficitur, libero metus bibendum arcu, auctor mollis arcu erat quis erat. Nunc tristique, metus
-					nec feugiat congue, ante lectus ultrices enim, et consequat mauris justo vitae erat. Duis vel
-					nunc
-					euismod, iaculis orci vel, vestibulum metus.
+					<?= get_property($lang,'offer_three_content') ?>
 				</p>
 			</div>
 		</div>
